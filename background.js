a809f9fde803b8a2a4db331f4c40ce5e60202765
Piotr Rusinek <piotr3k1447@gmail.com>
2022-08-27 20:08:54 +0200

    Bumped to manifest v3

--- background.js
+++ background.js
@@ -1,47 +1,41 @@
-function getJSON(url, cb) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.onreadystatechange = () => {
-    if (xhr.status != 200 || xhr.readyState != 4) return;
-    cb(JSON.parse(xhr.responseText));
-  }
-  xhr.send();
-};
-
 var data = [];
-let channels = [];
+var channels = [];
 
-(function update() {
-  getJSON("https://patostreamy.com/api/channels", (response) => {
-    const active = response.filter((channel) => channel.online);
-    data = active.map(({ title, thumbnail, streamUrl, platform, viewers }) => ({ title, thumbnail, streamUrl, platform, viewers }));
-    const names = active.map(({ title }) => title);
-    const fresh = names.filter(name => !channels.includes(name));
+(async function update() {
+	const fetchUrl = await fetch('https://patostreamy.com/api/channels');
+	const response = await fetchUrl.json();
 
-    const count = names.length;
-    const badge = count > 0 ? String(count) : "";
-    const popup = count > 0 ? "popup.html" : "";
+	const active = response.filter((channel) => channel.online);
+	data = active.map(({ title, thumbnail, streamUrl, platform, viewers }) => ({ title, thumbnail, streamUrl, platform, viewers }));
+	const names = active.map(({ title }) => title);
+	const fresh = names.filter(name => !channels.includes(name));
 
-    chrome.browserAction.setBadgeText({ text: badge });
-    chrome.browserAction.setPopup({ popup: popup });
-    chrome.browserAction.setTitle({ title: names.length > 1 ? `Aktualnie nadają: ${names.join(", ")}` : `${names[0] || 'Nikt aktualnie nie'} nadaje` });
+	const count = names.length;
+	const badge = count > 0 ? String(count) : "";
+	const popup = count > 0 ? "popup.html" : "";
 
-    if (fresh.length) {
-      chrome.storage.sync.get(['notifications'], function (result) {
-        if (result.notifications) {
-          const text = fresh.length > 1 ? `${fresh.join(", ")} rozpoczęli transmisje` : `${fresh[0]} zaczął transmisję`;
-          const options = {
-            type: "basic",
-            title: "Patostreamy",
-            message: text,
-            iconUrl: "icons/icon128.png"
-          };
-          chrome.notifications.create('patostream', options);
-        }
-      });
-    }
-    channels = names;
+	chrome.action.setBadgeText({ text: badge });
+	chrome.action.setPopup({ popup: popup });
+	chrome.action.setTitle({ title: names.length > 1 ? `Aktualnie nadają: ${names.join(", ")}` : `${names[0] || 'Nikt aktualnie nie'} nadaje` });
 
-    setTimeout(update, 60000);
-  });
+	if (fresh.length) {
+		chrome.storage.sync.get(['notifications'], function (result) {
+			if (result.notifications) {
+				const text = fresh.length > 1 ? `${fresh.join(", ")} rozpoczęli transmisje` : `${fresh[0]} zaczął transmisję`;
+				const options = {
+					type: "basic",
+					title: "Patostreamy",
+					message: text,
+					iconUrl: "icons/icon128.png"
+				};
+				chrome.notifications.create('patostream', options);
+			}
+		});
+	}
+	channels = names;
+
+	data = JSON.stringify(data);
+	chrome.storage.sync.set({data});
+
+	setTimeout(update, 60000);
 })();
--- popup.js
+++ popup.js
@@ -1,14 +1,16 @@
 window.onload = () => {
-  const data = chrome.extension.getBackgroundPage().data;
-  document.getElementById("list").innerHTML = "";
-  data.forEach((channel) => {
-    const text = `<li><div><a target="_blank" href="${channel.streamUrl}"><img src="${channel.thumbnail}" title="${channel.platform}" /></a></div><div><a target="_blank" href="${channel.streamUrl}">${channel.title}</a><br><i>${channel.viewers} widzów</i></div></li>`;
-    document.getElementById("list").innerHTML += text;
-  });
-  chrome.storage.sync.get(['notifications'], function (result) {
-    document.getElementById("notifications").checked = result.notifications;
-  });
-  document.getElementById("notifications").addEventListener('change', function () {
-    chrome.storage.sync.set({ notifications: this.checked });
-  });
+	chrome.storage.sync.get('data', (result) => {
+		const data = JSON.parse(result.data);
+		document.getElementById("list").innerHTML = "";
+		data.forEach((channel) => {
+			const text = `<li><div><a target="_blank" href="${channel.streamUrl}"><img src="${channel.thumbnail}" title="${channel.platform}" /></a></div><div><a target="_blank" href="${channel.streamUrl}">${channel.title}</a><br><i>${channel.viewers} widzów</i></div></li>`;
+			document.getElementById("list").innerHTML += text;
+		});
+	});
+	chrome.storage.sync.get(['notifications'], function (result) {
+		document.getElementById("notifications").checked = result.notifications;
+	});
+	document.getElementById("notifications").addEventListener('change', function () {
+		chrome.storage.sync.set({ notifications: this.checked });
+	});
 };
